f97b4110d250251b1c43abd71fded5933c21a39c
Jesse Yeboah <jesseyeboah16@gmail.com>
2025-04-12 22:32:52 +0000

    create folder structure

--- backend-service/src/app.module.ts
+++ backend-service/src/app.module.ts
@@ -1,9 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from './auth/auth.module';
-import { UserModule } from './user/user.module';
-import { BookmarkModule } from './bookmark/bookmark.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule],
+  imports: [],
 })
 export class AppModule {}
